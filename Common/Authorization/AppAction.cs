9eea540c3eda9d3333e7d11674f87b95235562fc
Bibek Pandey <bibek.pandey@dishhome.com.np>
2026-04-06 10:59:55 +0545

    feat(authorization);Created AppClaim

---
+++ Common/Authorization/AppAction.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Authorization
+{
+    public static class AppAction
+    {
+        public const string Read = nameof(Read);
+        public const string Write = nameof(Write);
+        public const string Delete = nameof(Delete);
+        public const string Update = nameof(Update);
+    }
+}
---
+++ Common/Authorization/AppFeature.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Authorization
+{
+    public class AppFeature
+    {
+        public const string Employees = nameof(Employees);
+        public const string Users = nameof(Users);
+        public const string Roles = nameof(Roles);
+        public const string UsersRoles = nameof(UsersRoles);
+        public const string RoleClaims = nameof(RoleClaims);
+
+    }
+}
